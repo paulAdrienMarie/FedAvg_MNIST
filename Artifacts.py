a42c1f01ad165a6d75943e980dfa42755cb6eeea
Tarek Ziadé <tarek@ziade.org>
2024-08-26 09:27:50 +0100

    first pass

--- Artifacts.py
+++ Artifacts.py
@@ -1,43 +1,47 @@
+# rename file to artifacts.py (see pep8)
+
 import platform
 import onnx
 from onnxruntime.training import artifacts
 import os
 import torch
 import io
+
 assert list(platform.python_version_tuple())[:-1] == ["3", "9"]
+
 
 class MNISTNET(torch.nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(MNISTNET, self).__init__()
-        
-        self.fc1 = torch.nn.Linear(input_size,hidden_size)
+
+        self.fc1 = torch.nn.Linear(input_size, hidden_size)
         self.relu = torch.nn.ReLU()
         self.fc2 = torch.nn.Linear(hidden_size, num_classes)
-        
-        
+
     def forward(self, model_input):
+        # this could be a one liner
         out = self.fc1(model_input)
         out = self.relu(out)
         out = self.fc2(out)
         return out
 
-class Artifacts: 
-    
+
+class Artifacts:
     def __init__(self, path_to_model):
         self.path_to_model = path_to_model
-          
-    def export_model(self):
 
+    def export_model(self):
         device = "cpu"
+        # use constants
         batch_size, input_size, hidden_size, output_size = 64, 784, 500, 10
         pt_model = MNISTNET(input_size, hidden_size, output_size).to(device)
-        
-        model_inputs = (torch.randn(batch_size,input_size,device=device),)
-        
+
+        model_inputs = (torch.randn(batch_size, input_size, device=device),)
+
         input_names = ["input"]
         output_names = ["output"]
         dynamic_axes = {"input": {0: "batch_size"}, "output": {0: "batch_size"}}
-        
+
         f = io.BytesIO()
         torch.onnx.export(
             pt_model,
@@ -50,23 +54,25 @@ class Artifacts:
             training=torch.onnx.TrainingMode.TRAINING,
             dynamic_axes=dynamic_axes,
             export_params=True,
-            keep_initializers_as_inputs=False
+            keep_initializers_as_inputs=False,
         )
-        
-        
+
         onnx_model = onnx.load_model_from_string(f.getvalue())
-        
-        onnx.save_model(onnx_model,self.path_to_model)
-        
+
+        onnx.save_model(onnx_model, self.path_to_model)
+
     def gen_artifacts(self):
-        
         onnx_model = onnx.load_model(self.path_to_model)
-        
+
         requires_grad = [param.name for param in onnx_model.graph.initializer]
-        frozen_params = [param.name for param in onnx_model.graph.initializer if param.name not in requires_grad]
-        
+        frozen_params = [
+            param.name
+            for param in onnx_model.graph.initializer
+            if param.name not in requires_grad
+        ]
+
         output_names = ["output"]
-        
+
         artifacts.generate_artifacts(
             onnx_model,
             optimizer=artifacts.OptimType.AdamW,
@@ -74,13 +80,15 @@ class Artifacts:
             requires_grad=requires_grad,
             frozen_params=frozen_params,
             additional_output_names=output_names,
-            artifact_directory=os.path.join(os.path.dirname(__file__), "artifacts")
+            # this should be a paramater as well
+            artifact_directory=os.path.join(os.path.dirname(__file__), "artifacts"),
         )
-        
+
     def __call__(self):
         self.export_model()
         self.gen_artifacts()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     obj = Artifacts("./onnx/inference.onnx")
     obj()
--- Federated.py
+++ Federated.py
@@ -1,3 +1,5 @@
+# rename file to federated.py
+
 import argparse
 from FederatedPreparer import FederatedPreparer
 from selenium import webdriver
@@ -7,46 +9,51 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class Federated:
     """
     Prepare the Federated Learning and launch it in headless mode on Firefox
     Server has to be launched previously
-    
+
     Attributs:
     nb_users -- Number of users for the Federated Learning
-    batch_size -- Size of the subset to attribute to one client 
+    batch_size -- Size of the subset to attribute to one client
     communication_round -- Number of model updates
-    federated_preparer -- Instance of the FederatedPreparer class 
+    federated_preparer -- Instance of the FederatedPreparer class
     url -- URL of the web application
     """
-    
-    def __init__(self,args):
+
+    def __init__(self, args):
         """
         Initializes a new instance of the Federated class
-        
+
         Arguments:
         args -- arguments passed at the execution of the script
         """
-        
+
         self.nb_users = args.nb_users
         self.batch_size = args.batch_size
         self.communication_round = args.nb_roc
-        self.federated_preparer = FederatedPreparer(self.nb_users,self.batch_size)
-        self.url = 'http://localhost:8080'
-        
-        
+        self.federated_preparer = FederatedPreparer(self.nb_users, self.batch_size)
+        # make it an optional argument + CONSTANT
+        self.url = "http://localhost:8080"
+
     def launch_federated_headless(self, url):
         """
-        Connects to the web web page and launches the simulation by clicking 
+        Connects to the web web page and launches the simulation by clicking
         on the launch federated button
-        
+
         Arguments:
         url -- URL of the web application
         """
 
         options = Options()
-        options.add_argument('--headless')  # Run in headless mode for no UI
-        service = Service('/usr/local/bin/geckodriver')  # Update this path to your WebDriver
+        options.add_argument("--headless")  # Run in headless mode for no UI
+        # make the path a constant at the top of the file
+        # you can also use shutil.which to try to find geckodriver
+        service = Service(
+            "/usr/local/bin/geckodriver"
+        )  # Update this path to your WebDriver
         driver = webdriver.Firefox(service=service, options=options)
 
         try:
@@ -55,15 +62,17 @@ class Federated:
 
             # Wait until the page is loaded and a specific element is present (modify the selector as needed)
             WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.TAG_NAME, 'body'))  # You can change this to a more specific element
+                EC.presence_of_element_located(
+                    (By.TAG_NAME, "body")
+                )  # You can change this to a more specific element
             )
 
             # Example of filling out input fields
-            user_field = driver.find_element(By.ID, 'nb_users')
+            user_field = driver.find_element(By.ID, "nb_users")
             user_field.clear()  # Clear any existing text
             user_field.send_keys(str(self.nb_users))  # Enter number of users
-            
-            communication_round_field = driver.find_element(By.ID, 'nb_roc')
+
+            communication_round_field = driver.find_element(By.ID, "nb_roc")
             communication_round_field.clear()
             communication_round_field.send_keys(str(self.communication_round))
 
@@ -71,15 +80,17 @@ class Federated:
             # user_field.send_keys(Keys.RETURN)
 
             # Click the launch button
-            button = driver.find_element(By.ID, 'launch_federated')
+            button = driver.find_element(By.ID, "launch_federated")
             if button:
                 print("Button found")
-            
+
             button.click()
 
             # Wait for the completion of training
             WebDriverWait(driver, 600).until(
-                EC.presence_of_element_located((By.ID, 'completion_element_id'))  # Modify to the appropriate element ID
+                EC.presence_of_element_located(
+                    (By.ID, "completion_element_id")
+                )  # Modify to the appropriate element ID
             )
 
             print("Training Completed")
@@ -90,24 +101,21 @@ class Federated:
         finally:
             # Clean up and close the browser
             driver.quit()
-            
-        
+
     def __call__(self):
         """Prepare and launch the Federated Learning"""
         self.federated_preparer()
         self.launch_federated_headless(self.url)
-        
+
+
 if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser(
-        prog="Launch Federated"
-    )
-    
+    parser = argparse.ArgumentParser(prog="Launch Federated")
+
     parser.add_argument("--nb_users", type=int, help="Number of users")
     parser.add_argument("--batch_size", type=int, help="Size of user batch of images")
     parser.add_argument("--nb_roc", type=str, help="Number of round of communication")
-    
+
     args = parser.parse_args()
-    
+
     obj = Federated(args)
     obj()
